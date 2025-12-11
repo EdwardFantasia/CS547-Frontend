9b27b3148b2ac1574a1f5d09d3f1e9ea81f54f88
Edward Fantasia <edward.fantasia@aceso.com>
2025-12-10 21:52:30 -0500

    Adjusted for refactored flask server, abount to deploy n vercel

--- app/Song.ts
+++ app/Song.ts
@@ -1,5 +1,6 @@
 export interface Song{
     track_name: string,
     track_id: string,
-    artist_name: string
+    artist_name: string,
+    time_s: number
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client"
-import Image from "next/image";
-import Link from 'next/link'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import SongComp from "./SongComp";
 import { Song } from "./Song";
 import Script from "next/script";
@@ -26,59 +24,59 @@ declare global {
 
 export default function Home() {
   const [songs, setSongs] = useState<Song[]>([])
-  const [fullSearchableSongs, setFullSearchableSongs] = useState<Song[]>([])
   const [searchableSongs, setSearchableSongs] = useState<Song[]>([])
   const [error, setError] = useState(false)
   const [songChosen, setSongChosen] = useState(false)
   const [chosenSong, setChosenSong] = useState<string>("")
   const [playingSong, setPlayingSong] = useState<string>("")
-  let spotifyController = null
+  const spotifyControllerRef = useRef<any>(null)
+  const [searchStarted, setSearchStarted] = useState<boolean>(false)
 
-  useEffect(() => {
-    const fetchSearchableSongs = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/getSongs", {
-        method: "GET",
+  useEffect(() => { //TODO: switch fetchSearchableSongs to a method where it happens on click and gets the string from input box and then returns songs and does basically same thing
+    window.onSpotifyIframeApiReady = (IFrameAPI) => {
+        const element = document.getElementById('spotify-embed-iframe');
+        const options = {
+            width: '100%',
+            height: 300,
+            uri: '', 
+        };
+
+        IFrameAPI.createController(element, options, (EmbedController) => {
+            // Store the controller instance globally or in component state/ref
+            spotifyControllerRef.current = EmbedController;
+            console.log("Spotify Embed Controller is Ready.");
+        });
+    };
+  }, [])
+
+  async function filterSearchable(){
+    const inputString: string = (document.getElementById("songInput") as HTMLInputElement).value.toLowerCase()
+    
+    try{
+        const response = await fetch("http://localhost:5000/searchSongs", {
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
           },
+          body: JSON.stringify(
+              {
+                mood: inputString
+              })
         });
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const respJson = await response.json();
         console.log(respJson)
-        setFullSearchableSongs(respJson)
+        setSearchStarted(true)
         setSearchableSongs(respJson)
-      } catch (err) {
-        //setSongs([{"artist": "Test artist", "id": "1000", "name": "Test name"}, {"artist": "Test artist longer than last name", "id": "1000", "name": "Test name"}])
-        setError(true);
-      }
-    };
-
-    fetchSearchableSongs();
-
-    // This function is defined ONCE in your main component/script
-    window.onSpotifyIframeApiReady = (IFrameAPI) => {
-        const element = document.getElementById('spotify-embed-iframe');
-        const options = {
-            width: '100%',
-            height: 300,
-            uri: 'spotify:track:5SuOikwiRyPMVoIQDJUgSV', 
-        };
-
-        // Create the controller ONCE
-        IFrameAPI.createController(element, options, (EmbedController) => {
-            // Store the controller instance globally or in component state/ref
-            spotifyController = EmbedController; 
-            console.log("Spotify Embed Controller is Ready.");
-        });
-    };
-  }, [])
-
-  function filterSearchable(){
-    const inputString: string = (document.getElementById("songInput") as HTMLInputElement).value.toLowerCase()
-    fullSearchableSongs.filter(song => song.artist_name.toLowerCase() === inputString || song.track_name.toLowerCase() === inputString)
+    }
+    catch (error){
+      console.log(error)
+      //setSongs([{"artist": "Test artist", "id": "1000", "name": "Test name"}, {"artist": "Test artist longer than last name", "id": "1000", "name": "Test name"}])
+      setSongs([])
+      setError(true)
+    }
   }
   
   async function onClick(){
@@ -146,9 +144,8 @@ export default function Home() {
 
   function onPlayableClick(songData: Song){
     console.log("playable")
+    spotifyControllerRef.current.loadUri(`spotify:track:${songData["track_id"]}`)
     setPlayingSong(songData["track_id"])
-    
-
   }
 
   async function onLikeOrSkip(evt: React.MouseEvent<HTMLButtonElement>){
@@ -187,28 +184,29 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-50 my-auto justify-center items-center font-sans dark:bg-black">
       <Script src="https://open.spotify.com/embed/iframe-api/v1" async></Script>
-      <div id = "spotify-embed-iframe"></div>
-      {fullSearchableSongs.length != 0 &&
+      <div id = "spotify-embed-iframe" ></div>
       <div id = "app-container">
         {songChosen == false &&
           <div className="flex flex-col mb-13">
-            <p>What song would you like recommended songs to sound like?</p>
+            <p>Type in the name of a song you want results to sound like:</p>
             <input className="outline-2 outline-offset-2 outline-solid outline-white rounded-sm my-2" id = "songInput"></input>
-            <button onClick = {() => filterSearchable()} className = "outline-2 outline-offset-2 outline-solid outline-white rounded-sm cursor-pointer my-2">Search For Song</button>
-              <div className="h-100 overflow-y-scroll px-[5px]">
-              {searchableSongs.map(el => {
-                  return(
-                    <SongComp selected = {el["track_id"] === chosenSong} cName = {"searchable"} key = {el["track_id"]} songCompClick = {() => onSearchableClick(el)} songData={el} ></SongComp>
-                  )
-                })
-              }
-            </div>
+            <button onClick = {() => filterSearchable()} className = "outline-2 outline-offset-2 outline-solid outline-white rounded-sm cursor-pointer my-2">Search For Songs</button>
+              {searchStarted &&
+                <div className="h-100 overflow-y-scroll px-[5px]">
+                  {searchableSongs.map(el => {
+                      return(
+                        <SongComp selected = {el["track_id"] === chosenSong} cName = {"searchable"} key = {el["track_id"]} songCompClick = {() => onSearchableClick(el)} songData={el} ></SongComp>
+                      )
+                    })
+                  }
+              </div>
+            }
           </div>
         }
         <div className="flex w-full items-center justify-center">
-          <label className = "mr-2" htmlFor = "mood">Sad / Less Energetic</label>
+          <label className = "mr-2" htmlFor = "mood">More Lyrical</label>
           <input className = "my-2" type="range" id="mood" name="mood" min="0" max="100" defaultValue="50" step="1" />
-          <label className = "ml-2" htmlFor = "mood">Happy / More Energetic</label>
+          <label className = "ml-2" htmlFor = "mood">More BPM-Based</label>
         </div>
         <div className="flex w-full flex-wrap items-center justify-center my-2">
           <button onClick = {() => onClick()} className = "outline-2 outline-offset-2 outline-solid outline-white rounded-sm cursor-pointer" id = "submit-btn">Search for Songs Matching My Mood</button>
@@ -234,7 +232,6 @@ export default function Home() {
           }
         </div>
       </div>
-    }
     </div>
   );
 }
